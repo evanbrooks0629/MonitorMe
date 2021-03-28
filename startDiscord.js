1b6e731390750e595d3883d44a4cced6705ea444
evanbrooks0629 <60624108+evanbrooks0629@users.noreply.github.com>
2021-03-27 23:02:52 -0400

    Update startDiscord.js

--- startDiscord.js
+++ startDiscord.js
@@ -2,8 +2,7 @@ const Discord = require('discord.js');
 
 const startDiscord = () => {
 
-    const token = "ODI1NTQ1MjU5MDY0NDkyMDMz.YF_e7w.h_4SiIRrs8ezxwoPO9p_HZThkck";
-    //const channelID = "820422259323699203";
+    const token = "token ";
     const client = new Discord.Client();
     
     client.once('ready', async () => {
